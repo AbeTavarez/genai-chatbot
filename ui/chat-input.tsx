0be4e8974cddefde9df0c9ac4841af3fed16c46d
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-07-09 23:33:44 -0400

    Added state and event handlers for user interactions

--- ui/chat-input.tsx
+++ ui/chat-input.tsx
@@ -1,13 +1,24 @@
+"use client";
+import { ChangeEvent, FormEvent } from "react";
 
+type ChatProps = {
+  userMessage: string;
+  setUserMessage: (value: string) => void;
+  handleSendMessage: () => void;
+};
 
-export default function ChatInput() {
+export default function ChatInput({ userMessage, setUserMessage, handleSendMessage }: ChatProps) {
   return (
     <div className=" bg-slate-950 p-3">
       <div className="flex space-x-2 items-center  bg-slate-600 mt-auto">
-        <form className="flex items-center justify-center w-full space-x-2">
+        <form onSubmit={handleSendMessage} className="flex items-center justify-center w-full space-x-2">
           <input
+            value={userMessage}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setUserMessage(e.target.value)
+            }
             type="text"
-            placeholder=" Type your message"
+            placeholder="Type your message"
             className="flex h-10 w-full rounded-md border border-[#e5e7eb] px-3 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
           />
 
