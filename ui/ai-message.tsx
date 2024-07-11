a35f50bdec4d574a139a10184200d2b2b15f5db2
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-07-10 23:27:06 -0400

    message component props

--- ui/ai-message.tsx
+++ ui/ai-message.tsx
@@ -1,14 +1,15 @@
 import { RiRobot3Line } from "react-icons/ri";
+import { Message } from "@/components/chatbot";
 
-export default function AIMessage() {
+export default function AIMessage({ role, content }: Message) {
   return (
     <div id="bot" className="flex min-w-96 my-2">
-      <div className="flex justify-center p-1 w-8 h-8 border rounded-full bg-slate-600">
+      <div className="flex justify-center p-1 w-8 h-8 border bg-slate-600 rounded-full">
         <RiRobot3Line size={18} />
       </div>
       <div className="ml-2">
-        <div>Bot</div>
-        <p>Hello, how can i help you today?</p>
+        <div>{role}</div>
+        <p>{content}</p>
       </div>
     </div>
   );
--- ui/user-message.tsx
+++ ui/user-message.tsx
@@ -1,13 +1,15 @@
 import { CiUser } from "react-icons/ci";
-export default function UserMessage() {
+import { Message } from "@/components/chatbot";
+
+export default function UserMessage({ role, content }: Message) {
   return (
     <div id="user" className="flex min-w-96 my-2">
       <div className="flex justify-center p-1 w-8 h-8 border rounded-full bg-slate-600">
         <CiUser size={18} />
       </div>
       <div className="ml-2">
-        <div>You</div>
-        <p>I need help with my order</p>
+        <div>{role}</div>
+        <p>{content}</p>
       </div>
     </div>
   );
