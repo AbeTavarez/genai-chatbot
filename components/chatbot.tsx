d3cf83adac35829c49bb249a5245199806e6e5b2
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-07-10 23:28:57 -0400

    mapping messages, openai updated to latest api

--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -4,28 +4,39 @@ import ChatInput from "@/ui/chat-input";
 import UserMessage from "@/ui/user-message";
 import { FormEvent, useState } from "react";
 import { TbMessageChatbot } from "react-icons/tb";
-import {chatCompletion} from '@/actions'
-type Message = {
-    text: string;
-    sender: string
-}
+import { chatCompletion } from "@/actions";
+
+export type Message = {
+  content: string;
+  role: string;
+};
 
 export default function Chatbot() {
   const [showChat, setShowChat] = useState(false);
-  const [userMessage, setUserMessage] = useState('');
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [userMessage, setUserMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([
+    { role: "assistant", content: "Hello, how can i help you today?" },
+  ]);
 
+  /**
+   * Send User messages
+   * @param e
+   * @returns
+   */
   const handleSendMessage = async (e: FormEvent) => {
-      console.log(userMessage);
-    e.preventDefault()
+    console.log(userMessage);
+    e.preventDefault();
     if (!userMessage) return;
 
-    const newMessage: Message = {text: userMessage, sender: 'user'}
+    const newMessage: Message = { content: userMessage, role: "user" };
     setMessages([...messages, newMessage]);
     // show loading
+    console.log("HERE", messages);
+    
     const res = await chatCompletion(messages);
-    console.log(res.choices[0].message.content);
-  }
+    console.log(res);
+    // console.log(res.choices[0].message.content);
+  };
 
   return (
     <>
@@ -33,13 +44,12 @@ export default function Chatbot() {
       <TbMessageChatbot
         size={64}
         onClick={() => setShowChat(!showChat)}
-        className="fixed right-24 bottom-[calc(1rem)]"
+        className="fixed right-24 bottom-[calc(1rem)] hover:cursor-pointer"
       />
 
       {/* CHAT  */}
       {!showChat && (
         <div className="fixed right-24 bottom-[calc(4rem+1.5rem)] hover:cursor-pointer border p-5 shadow-md shadow-white h-[474px]">
-
           <div className="flex flex-col h-full">
             {/* CHAT HEADER */}
             <div>
@@ -49,16 +59,23 @@ export default function Chatbot() {
 
             {/* CHAT CONTAINER  */}
             <div className="flex flex-col flex-1 items-center justify-center p-2 mt-5  overflow-y-auto">
-              <AIMessage />
-              <UserMessage />
+              {messages &&
+                messages.map((m) => {
+                  return m.role === "assistant" ? (
+                    <AIMessage {...m} />
+                  ) : (
+                    <UserMessage {...m} />
+                  );
+                })}
             </div>
 
             {/* INPUT FORM  */}
-            <ChatInput userMessage={userMessage} setUserMessage={setUserMessage}
-            handleSendMessage={handleSendMessage}
+            <ChatInput
+              userMessage={userMessage}
+              setUserMessage={setUserMessage}
+              handleSendMessage={handleSendMessage}
             />
           </div>
-
         </div>
       )}
     </>
