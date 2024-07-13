daaac9a2423024a9634b952b2765107a8f394085
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-07-12 23:11:07 -0400

    fixed element width

--- ui/ai-message.tsx
+++ ui/ai-message.tsx
@@ -3,7 +3,7 @@ import { Message } from "@/components/chatbot";
 
 export default function AIMessage({ role, content }: Message) {
   return (
-    <div id="bot" className="flex min-w-96 my-2">
+    <div id="bot" className="flex w-full my-2">
       <div className="flex justify-center p-1 w-8 h-8 border bg-slate-600 rounded-full">
         <RiRobot3Line size={18} />
       </div>
--- ui/user-message.tsx
+++ ui/user-message.tsx
@@ -3,7 +3,7 @@ import { Message } from "@/components/chatbot";
 
 export default function UserMessage({ role, content }: Message) {
   return (
-    <div id="user" className="flex min-w-96 my-2">
+    <div className="flex w-full my-2">
       <div className="flex justify-center p-1 w-8 h-8 border rounded-full bg-slate-600">
         <CiUser size={18} />
       </div>
