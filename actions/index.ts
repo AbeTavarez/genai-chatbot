3945d338f46ccdc8b1f0aa491cd0309f52e11c62
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-07-18 23:23:44 -0400

    chat completion docs

--- actions/index.ts
+++ actions/index.ts
@@ -7,16 +7,25 @@ const openAI = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+/**
+ * Chat Completion
+ * @param chatMessages 
+ * @returns new message
+ */
 export async function chatCompletion(chatMessages: Message[]) {
+  // create chat with prompt
   const chat = [
     { role: "system", content: "You're a helpful assistance" },
     ...chatMessages,
   ];
 
+  // API Request
   const completion = await openAI.chat.completions.create({
     messages: chat,
     model: "gpt-3.5-turbo",
   });
+
+  // completion result (new message)
   console.log(completion.choices[0]);
   return completion; // or return jys the result?
 }
