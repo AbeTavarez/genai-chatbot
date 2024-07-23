a61a87eac9f7f931fd2870ca502625c9e682ed1b
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-07-22 23:34:19 -0400

    added faq to api call for system context

--- actions/index.ts
+++ actions/index.ts
@@ -2,7 +2,7 @@
 import { Message } from "@/components/chatbot";
 import OpenAI from "openai";
 //  --> 1. import modules
-import fs from 'fs';
+import fs from "fs";
 import path from "path";
 
 // === OPEN AI
@@ -14,36 +14,42 @@ const openAI = new OpenAI({
 type FAQ = {
   question: string;
   answer: string;
-}
+};
 
 // --> 2. Load json file
-const filePath = path.resolve(process.cwd(), 'data', 'faqs.json');
-const faqs: FAQ[] = JSON.parse(fs.readFileSync(filePath, 'utf-8')).faqs;
+const filePath = path.resolve(process.cwd(), "data", "faqs.json");
+const faqs: FAQ[] = JSON.parse(fs.readFileSync(filePath, "utf-8")).faqs;
 // console.log(faqs);
-
-
-
 
 /**
  * Chat Completion
- * @param chatMessages 
+ * @param chatMessages
  * @param newMessage
  * @returns new message from assistant
  */
-export async function chatCompletion(chatMessages: Message[], newMessage: Message) {
+export async function chatCompletion(
+  chatMessages: Message[],
+  newMessage: Message,
+) {
   // --> 3. check faqs for answer
-const faqAnswer = faqs.find(faq => newMessage.content.toLowerCase().includes(faq.question.toLowerCase()));
+  const faqAnswer = faqs.find((faq) =>
+    newMessage.content.toLowerCase().includes(faq.question.toLowerCase()),
+  );
 
-// -- 4.
-if (faqAnswer) {
-  console.log(faqAnswer);
-  
-  return {role: 'assistant', content: faqAnswer}
-}
+  // -- 4.
+  if (faqAnswer) {
+    console.log(faqAnswer);
+
+    return { role: "assistant", content: faqAnswer.answer };
+  }
 
   // create chat with prompt
   const chat = [
     { role: "system", content: "You're a helpful assistance" },
+    ...faqs.map((faq) => ({
+      role: "system",
+      content: `Q: ${faq.question}\nA: ${faq.answer}`,
+    })),
     ...chatMessages,
   ];
 
@@ -58,4 +64,3 @@ if (faqAnswer) {
   console.log(completion.choices[0]);
   return completion; // or return jys the result?
 }
-
