c00ccfe73c21d0904e527ef888f41bc1e7913256
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-07-26 23:38:14 -0400

    chat api route

--- app/chat/api/route.ts
+++ app/chat/api/route.ts
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic' // defaults to auto
+export const dynamic = "force-dynamic"; // defaults to auto
 
 import { Message } from "@/components/chatbot";
 import OpenAI from "openai";
@@ -9,20 +9,20 @@ const openAI = new OpenAI({
 });
 
 export async function POST(request: Request, response: Response) {
-    console.log(request.body);
-    const {chatMessages} = request.body;
-    
-    if (!chatMessages) return;
+  console.log(request.body);
+  const { chatMessages } = request.body;
 
-    const chat = [
-        { role: "system", content: "You're a helpful assistance" },
-        ...chatMessages,
-      ];
-    
-      const completion = await openAI.chat.completions.create({
-        messages: chat,
-        model: "gpt-3.5-turbo",
-      });
-      console.log(completion.choices[0]);
-      return completion; // or return jys the result?
+  if (!chatMessages) return response.json({ msg: "chat messages required" });
+
+  const chat = [
+    { role: "system", content: "You're a helpful assistance" },
+    ...chatMessages,
+  ];
+
+  const completion = await openAI.chat.completions.create({
+    messages: chat,
+    model: "gpt-3.5-turbo",
+  });
+  console.log(completion.choices[0]);
+  return completion; // or return jys the result?
 }
